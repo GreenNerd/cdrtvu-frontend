baf61d25b543f07b33fd2f19077ffc163c2fc62b
moonRider <solar.pom@gmail.com>
2015-05-29 00:13:08 +0800

    Change: let categories panel to be responsive on mobile

--- src/scripts/index.coffee
+++ src/scripts/index.coffee
@@ -4,6 +4,7 @@ $ ->
       @pcScroll()
       @mobileBars()
       @enableSubMenu()
+      @enableCategorySwitch()
 
     pcScroll: ->
       $(document).scroll (event)->
@@ -29,5 +30,43 @@ $ ->
         else
           $target.addClass('active')
           $relate.addClass('active')
+
+    enableCategorySwitch: ->
+      $container = $('#categories_container')
+      $control = $('#category_switch_control')
+      $prev = $control.find('.prev')
+      $next = $control.find('.next')
+
+      getCurrentIndex = ->
+        parseInt($container.data('index')) or 0
+
+      setControlStatus = (index)->
+        if index is 0
+          $prev.addClass('disabled')
+        else
+          $prev.removeClass('disabled')
+        if index is (count - 1)
+          $next.addClass('disabled')
+        else
+          $next.removeClass('disabled')
+
+      setCategoryIndex = (index)->
+        $container.data('index', index)
+        setControlStatus(index)
+        $container.css('margin-left', "-#{index}00%")
+
+      startIndex = getCurrentIndex()
+      count = $container.children('.category-item').length
+      setControlStatus(startIndex)
+
+      $control
+        .on 'click', '.prev', (event)->
+          return true if $(event.target).hasClass('disabled')
+          setCategoryIndex(getCurrentIndex() - 1)
+        .on 'click', '.next', (event)->
+          return true if $(event.target).hasClass('disabled')
+          setCategoryIndex(getCurrentIndex() + 1)
+
+
 
   IndexCtrl.init()
--- src_target/scripts/index.js
+++ src_target/scripts/index.js
@@ -4,7 +4,8 @@ $(function() {
     init: function() {
       this.pcScroll();
       this.mobileBars();
-      return this.enableSubMenu();
+      this.enableSubMenu();
+      return this.enableCategorySwitch();
     },
     pcScroll: function() {
       return $(document).scroll(function(event) {
@@ -36,6 +37,47 @@ $(function() {
           $target.addClass('active');
           return $relate.addClass('active');
         }
+      });
+    },
+    enableCategorySwitch: function() {
+      var $container, $control, $next, $prev, count, getCurrentIndex, setCategoryIndex, setControlStatus, startIndex;
+      $container = $('#categories_container');
+      $control = $('#category_switch_control');
+      $prev = $control.find('.prev');
+      $next = $control.find('.next');
+      getCurrentIndex = function() {
+        return parseInt($container.data('index')) || 0;
+      };
+      setControlStatus = function(index) {
+        if (index === 0) {
+          $prev.addClass('disabled');
+        } else {
+          $prev.removeClass('disabled');
+        }
+        if (index === (count - 1)) {
+          return $next.addClass('disabled');
+        } else {
+          return $next.removeClass('disabled');
+        }
+      };
+      setCategoryIndex = function(index) {
+        $container.data('index', index);
+        setControlStatus(index);
+        return $container.css('margin-left', "-" + index + "00%");
+      };
+      startIndex = getCurrentIndex();
+      count = $container.children('.category-item').length;
+      setControlStatus(startIndex);
+      return $control.on('click', '.prev', function(event) {
+        if ($(event.target).hasClass('disabled')) {
+          return true;
+        }
+        return setCategoryIndex(getCurrentIndex() - 1);
+      }).on('click', '.next', function(event) {
+        if ($(event.target).hasClass('disabled')) {
+          return true;
+        }
+        return setCategoryIndex(getCurrentIndex() + 1);
       });
     }
   };
