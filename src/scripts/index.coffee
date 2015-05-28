736b13e4d923b69af9f8205d42f38430bea3ba69
moonRider <solar.pom@gmail.com>
2015-05-28 21:08:19 +0800

    Add: header's scripts

--- src/scripts/index.coffee
+++ src/scripts/index.coffee
@@ -1,7 +1,33 @@
 $ ->
-  $(document).scroll (event)->
-    top = $(document).scrollTop()
-    if top < 30
-      $('#header_nav').addClass('expanded')
-    else
-      $('#header_nav').removeClass('expanded')
+  IndexCtrl =
+    init: ->
+      @pcScroll()
+      @mobileBars()
+      @enableSubMenu()
+
+    pcScroll: ->
+      $(document).scroll (event)->
+        top = $(document).scrollTop()
+        if top < 30
+          $('#header_nav').addClass('expanded')
+        else
+          $('#header_nav').removeClass('expanded')
+
+    mobileBars: ->
+      $('#menu_icon').on 'click', (event)->
+        $('#header_nav_mobile').toggleClass('expanded')
+
+    enableSubMenu: ->
+      $('#header_nav_mobile').on 'click', '.has-sub-nav', (event)->
+        event.stopPropagation()
+        event.preventDefault()
+        $target = $(event.target).closest('a')
+        $relate = $ $target.attr('href')
+        if $target.hasClass('active')
+          $relate.removeClass('active')
+          $target.removeClass('active')
+        else
+          $target.addClass('active')
+          $relate.addClass('active')
+
+  IndexCtrl.init()
--- src_target/scripts/index.js
+++ src_target/scripts/index.js
@@ -1,11 +1,43 @@
 $(function() {
-  return $(document).scroll(function(event) {
-    var top;
-    top = $(document).scrollTop();
-    if (top < 30) {
-      return $('#header_nav').addClass('expanded');
-    } else {
-      return $('#header_nav').removeClass('expanded');
+  var IndexCtrl;
+  IndexCtrl = {
+    init: function() {
+      this.pcScroll();
+      this.mobileBars();
+      return this.enableSubMenu();
+    },
+    pcScroll: function() {
+      return $(document).scroll(function(event) {
+        var top;
+        top = $(document).scrollTop();
+        if (top < 30) {
+          return $('#header_nav').addClass('expanded');
+        } else {
+          return $('#header_nav').removeClass('expanded');
+        }
+      });
+    },
+    mobileBars: function() {
+      return $('#menu_icon').on('click', function(event) {
+        return $('#header_nav_mobile').toggleClass('expanded');
+      });
+    },
+    enableSubMenu: function() {
+      return $('#header_nav_mobile').on('click', '.has-sub-nav', function(event) {
+        var $relate, $target;
+        event.stopPropagation();
+        event.preventDefault();
+        $target = $(event.target).closest('a');
+        $relate = $($target.attr('href'));
+        if ($target.hasClass('active')) {
+          $relate.removeClass('active');
+          return $target.removeClass('active');
+        } else {
+          $target.addClass('active');
+          return $relate.addClass('active');
+        }
+      });
     }
-  });
+  };
+  return IndexCtrl.init();
 });
